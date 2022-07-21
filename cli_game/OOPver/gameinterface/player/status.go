28b2157edd22bd4343d450999bceb1b29a94cf78
nyhwbh <87648321+nyhwbh@users.noreply.github.com>
2022-07-21 22:37:47 +0900

    fallow code convention, add species setup, player level up

--- cli_game/OOPver/gameinterface/player/status.go
+++ cli_game/OOPver/gameinterface/player/status.go
@@ -18,16 +18,24 @@ type PlayerStatus struct {
 	species int
 }
 
-func (c *PlayerStatus) BasicSetup() {
+func (p *PlayerStatus) BasicSetup() {
 	timeSource := rand.NewSource(time.Now().UnixNano())
 	random := rand.New(timeSource)
-	c.level = 1
-	c.avoid = 30
-	c.maxhp = random.Intn(50) + 50
-	c.hp = c.maxhp
-	c.maxmp = random.Intn(50) + 50
-	c.mp = c.maxmp
-	c.atk = random.Intn(10) + 20
-	c.def = random.Intn(10) + 10
-	c.atks = 1000
+	p.level = 1
+	p.avoid = 30
+	p.maxhp = random.Intn(50) + 50
+	p.hp = p.maxhp
+	p.maxmp = random.Intn(50) + 50
+	p.mp = p.maxmp
+	p.atk = random.Intn(10) + 20
+	p.def = random.Intn(10) + 10
+	p.atks = 1000
+}
+
+func (p PlayerStatus) PlayerSpeciesSetUp(sp int) {
+	p.species = sp
+}
+
+func (p *PlayerStatus) PlayerLevelUp() {
+	p.level += 1
 }
