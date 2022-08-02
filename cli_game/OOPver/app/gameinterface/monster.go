c2f9aacd8bf3693939d7a3db611dc5da35b003ef
nyhwbh <87648321+nyhwbh@users.noreply.github.com>
2022-08-02 16:59:23 +0900

    add monster interface

---
+++ cli_game/OOPver/app/gameinterface/monster.go
@@ -0,0 +1,34 @@
+package gameinterface
+
+import (
+	"math/rand"
+	"time"
+)
+
+type Monster interface {
+	CreateMonster()
+	PlayerAttack()
+}
+type MonsterStatus struct {
+	hp         int
+	mp         int
+	basicAtk   int
+	basicAtks  int
+	basicDef   int
+	basicAvoid int
+}
+
+func (m *MonsterStatus) CreateMonster() {
+	timeSource := rand.NewSource(time.Now().UnixNano())
+	random := rand.New(timeSource)
+	m.basicAvoid = 30
+	m.hp = random.Intn(50) + 50
+	m.mp = random.Intn(50) + 50
+	m.basicAtk = random.Intn(10) + 10
+	m.basicDef = random.Intn(10) + 5
+	m.basicAtks = 800
+}
+
+func (m *MonsterStatus) PlayerAttack(damage int) {
+	m.hp -= damage
+}
